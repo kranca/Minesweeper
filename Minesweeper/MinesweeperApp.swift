38a7db08516ebe44253cde1114e116d30a6a6853
kranca <90696292+kranca@users.noreply.github.com>
2022-08-24 08:59:28 +0200

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Raúl Carrancá on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
